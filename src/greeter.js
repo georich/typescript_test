afe814868ba984f50dacec8874eb96b8de7fa2cb
George Richards <g.richards101@gmail.com>
2018-04-23 12:48:16 +0100

    implement class

--- src/greeter.js
+++ src/greeter.js
@@ -1,10 +1,21 @@
 "use strict";
 exports.__esModule = true;
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 var greeter = function (person) {
-    return "Hello, " + person;
+    // return `Hello, ${person.firstName} ${person.middleInitial} ${person.lastName}`;
+    return "Hello " + person.fullName;
 };
-var user = {
-    firstName: 'George',
-    lastName: 'User'
-};
+// const user = {
+//   firstName: 'George',
+//   lastName: 'User'
+// };
+var user = new Student('George', 'T.', 'Richards');
 console.log(greeter(user));
--- src/greeter.ts
+++ src/greeter.ts
@@ -1,16 +1,29 @@
+class Student {
+  fullName: string;
+
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+  }
+}
+
 interface Person {
   firstName: string;
+  middleInitial: string;
   lastName: string;
+  fullName: string;
 }
 
 const greeter = (person: Person) => {
-  return `Hello, ${person}`;
+  // return `Hello, ${person.firstName} ${person.middleInitial} ${person.lastName}`;
+  return `Hello ${person.fullName}`;
 };
 
-let user = {
-  firstName: 'George',
-  lastName: 'User'
-};
+// const user = {
+//   firstName: 'George',
+//   lastName: 'User'
+// };
+
+const user = new Student('George', 'T.', 'Richards');
 
 console.log(greeter(user));
 
